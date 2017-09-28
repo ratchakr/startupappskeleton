0bd64ed2b1d4db09e31c991e5157cd85467bd824
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2017-09-28 00:42:52 -0500

    add java package

---
+++ src/main/java/com/startupapp/StartupappApplication.java
@@ -0,0 +1,12 @@
+package com.startupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupappApplication.class, args);
+	}
+}
